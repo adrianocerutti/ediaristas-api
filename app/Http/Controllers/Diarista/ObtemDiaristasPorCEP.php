310f1f4d9da5cf2a0d0033cec94a8fb026adaa4a
Adriano B. Cerutti <adrianocerutti@gmail.com>
2021-09-13 11:19:24 -0300

    Finalizada criação da primeira Action

--- app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
+++ app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Diarista;
 
+use App\Actions\Diarista\ObterDiaristasPorCEP;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\DiaristaPublicoCollection;
 use App\Services\ConsultaCep\ConsultaCEPInterface;
-use Illuminate\Validation\ValidationException;
 
 class ObtemDiaristasPorCEP extends Controller
 {
@@ -19,21 +19,17 @@ class ObtemDiaristasPorCEP extends Controller
      * @param ConsultaCEPInterface $servicoCEP
      * @return DiaristaPublicoCollection|JsonResponse
      */
-    public function __invoke(Request $request, ConsultaCEPInterface $servicoCEP): DiaristaPublicoCollection|JsonResponse
+    public function __invoke(Request $request, ObterDiaristasPorCEP $action): DiaristaPublicoCollection|JsonResponse
     {
         $request->validate([
             'cep' => ['required', 'numeric']
         ]);
 
-        $dados = $servicoCEP->buscar($request->cep);
-
-        if ($dados === false) {
-            throw ValidationException::withMessages(['cep' => 'CEP não encontrado']);
-        }
+        [$diaristasCollection, $quantidadeDiaristas] = $action->executar($request->cep);
 
         return new DiaristaPublicoCollection(
-            User::diaristasDisponivelCidade($dados->ibge),
-            User::diaristasDisponivelCidadeTotal($dados->ibge)
+            $diaristasCollection,
+            $quantidadeDiaristas
         );
     }
 }
