e471c77a5cedff767569686e36502280d4afbfd6
Adriano B. Cerutti <adrianocerutti@gmail.com>
2021-09-13 13:41:56 -0300

    Finalizada verificação de disponibilidade de diaristas para o CEP

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Diarista\ObtemDiaristasPorCEP;
+use App\Http\Controllers\Diarista\VerificaDisponibilidade;
 
 Route::get('/diaristas/localidades', ObtemDiaristasPorCEP::class)->name('diarista.busca_por_cep');
+Route::get('/diaristas/disponibilidade', VerificaDisponibilidade::class)->name('enderecos.disponibilidade');
