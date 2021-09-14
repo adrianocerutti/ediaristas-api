6f4252380ecc110353094a323f133be738713243
Adriano B. Cerutti <adrianocerutti@gmail.com>
2021-09-14 10:51:54 -0300

    Finalizado Hateoas inicial da API

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ObtemServicos;
+use App\Http\Controllers\IndexController;
 use App\Http\Controllers\Endereco\BuscaCepApiExterna;
 use App\Http\Controllers\Diarista\ObtemDiaristasPorCEP;
 use App\Http\Controllers\Diarista\VerificaDisponibilidade;
+
+Route::get('/', IndexController::class);
 
 Route::get('/diaristas/localidades', ObtemDiaristasPorCEP::class)->name('diarista.busca_por_cep');
 Route::get('/diaristas/disponibilidade', VerificaDisponibilidade::class)->name('enderecos.disponibilidade');
